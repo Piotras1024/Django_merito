9b8a491000a78694bbbbbf62ead163f5a99b33dc
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-02-19 13:11:54 +0100

    new beginning

--- project_flashcards/urls.py
+++ project_flashcards/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for flashcards project.
+URL configuration for project_flashcards project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- project_flashcards/wsgi.py
+++ project_flashcards/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flashcards project.
+WSGI config for project_flashcards project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flashcards.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_flashcards.settings')
 
 application = get_wsgi_application()
